09a6adc43575452e090a0ebec5994b114ee9efde
MMunsinger <matthew.munsinger@gmail.com>
2024-11-20 14:14:02 -0600

    remove comments and format

--- server-stats.sh
+++ server-stats.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
-
-## Get Cpu Name
 get_cpu_name() {
     echo "##### CPU INFO #####"
     cat /proc/cpuinfo | awk -F: 'NR==5 {printf "Cpu Name: %s\n", $2}'
     echo
 }
 
-## Get CPU Usage
 get_cpu_usage() {
     echo "##### CPU USAGE #####"
     mpstat | awk 'NR==4 {printf "User: %.2f%%, System: %.2f%%, IDLE: %.2f%%\n", $3, $5, $12}'
     echo
 }
 
-## Get MEM Usage
 get_mem_usage() {
     echo "##### MEM USAGE #####"
     free --mega | awk 'NR==2 {printf "Used: %s MB, Free: %s MB, Usage: %.1f%%\n", $3, $4, ($3/$2)*100}'
     echo
 }
 
-## Get Disk Usage
 get_disk_usage() {
     echo "##### Disk Usage #####"
-    df -h --total | awk '/^total/ {printf "Used: %s, Avail: %s, Total: %s, Usage: %i%%\n", $3, $4, $2, $5}' 
+    df -h --total | awk '/total/ {printf "Used: %s, Avail: %s, Total: %s, Usage: %i%%\n", $3, $4, $2, $5}' 
     echo
 }
 
-## Top 5 Proc by CPU
 get_top_cpu_processes() {
     echo "### Top 5 Processes by CPU Usage ###"
     ps -eo pid,comm,%cpu --sort=-%cpu | head -6
@@ -51,9 +45,7 @@ ec_INFO() {
     hostnamectl | awk '/Operating System/'
 }
 
-
-
-# Execute functions
+# Execute
 echo "Server Performance Stats"
 echo "========================="
 get_cpu_name
